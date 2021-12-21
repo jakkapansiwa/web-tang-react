bc43ebd4a123b9e969c0aafe5e9fe54289aa7297
jakkapan office <jakkapansiwa@gmail.com>
2021-12-21 14:53:31 +0700

    first 21/12

---
+++ src/components/AppHeader.js
@@ -0,0 +1,13 @@
+import './AppHeader.css';
+function AppHeader() {
+    return (
+        <header className="app-header">
+            <img className="app-header-logo" src="/images/logo-bulldog.jpg" />
+            {/* <h3 className="app-header-text">HU-TANG</h3> */}
+           
+        </header>
+        
+    );
+}
+
+export default AppHeader;
---
+++ src/data/tattoos.js
@@ -0,0 +1,15 @@
+const tattoos = [
+    { title: "แขนเท่ห์ๆ", thumbnailUrl: "/images/tattoo-01-small.jpg", fullUrl: "/images/tattoo-01-large.jpg" },
+    { title: "มือเท่ห์ๆ", thumbnailUrl: "/images/tattoo-02-small.jpg", fullUrl: "/images/tattoo-02-large.jpg" },
+    { title: "คอเท่ห์ๆ", thumbnailUrl: "/images/tattoo-03-small.jpg", fullUrl: "/images/tattoo-03-large.jpg" },
+    { title: "หลังเท่ห์ๆ", thumbnailUrl: "/images/tattoo-04-small.jpg", fullUrl: "/images/tattoo-04-large.jpg" },
+    { title: "แขนเท่ห์ๆ", thumbnailUrl: "/images/tattoo-05-small.jpg", fullUrl: "/images/tattoo-05-large.jpg" },
+    { title: "แขนเท่ห์ๆ", thumbnailUrl: "/images/tattoo-06-small.jpg", fullUrl: "/images/tattoo-06-large.jpg" },
+
+    { title: "tang_1", thumbnailUrl: "/images/tang_1.jpg", fullUrl: "/images/tang_1.jpg" },
+    { title: "tang_2", thumbnailUrl: "/images/tang_2.jpg", fullUrl: "/images/tang_2.jpg" },
+    { title: "tang_3", thumbnailUrl: "/images/tang_3.jpg", fullUrl: "/images/tang_3.jpg" },
+    { title: "tang_4", thumbnailUrl: "/images/tang_4.jpg", fullUrl: "/images/tang_4.jpg" },
+];
+
+export default tattoos;
